e2beacc103babf8d36ecb15295f9e70d0990f588
pjpirie <pauljohnpirie@yahoo.co.uk>
2018-11-10 23:15:29 +0000

    SVG Button

--- index.php
+++ index.php
@@ -10,6 +10,23 @@
 <body>
 	<div class="container">
 		<input class="main-input" type="text">
+		<button id="add">
+		 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 120 120">
+			 <defs>
+				 <style>
+					 .cls-1{
+						 fill:#adfc9b;
+					 }
+				 </style>
+			 </defs>
+			 <title>Add</title>
+			 <g id="Layer_2" data-name="Layer 2">
+				 <g id="Layer_2-2" data-name="Layer 2">
+					 <path class="cls-1" id="add-svg" d="M60,0a60,60,0,1,0,60,60A60,60,0,0,0,60,0ZM89.75,65.75h-25v24.5h-10V65.75h-25v-10h25V30.25h10v25.5h25Z"/>
+				 </g>
+			 </g>
+		</svg>
+		</button>
 	</div>
 </body>
 </html>
